8f5d5d7db434ad32ef6d19091d54ce1974e86e2e
gabriel <gabrielgoller123@gmail.com>
2020-11-04 17:47:53 +0100

    Cleaned up code and added comments

--- server/gui/src/main.rs
+++ server/gui/src/main.rs
@@ -19,38 +19,47 @@ fn get_degrees(mq: &PosixMq) -> f32 {
             mod_buf.push_str(&(i - 48).to_string());
         }
     }
-    println!("buf: {}", mod_buf);
     let degrees: i32 = mod_buf.parse::<i32>().unwrap();
     return degrees as f32;
 }
 
 fn main() {
+    // Create kiss3d window
     let mut window = Window::new("Kiss3d: cube");
-
+    // Set lights and stick to camera
     window.set_light(Light::StickToCamera);
+
+    // Open messagequeue
     let mq = PosixMq::open("/degree").unwrap();
     let mut counter = 0;
     let mut degrees = 0.0;
+    // Create mspc channel to communicate between threads
     let (tx, rx) = mpsc::channel();
 
+    // Create thread which gets degrees from mq
     thread::spawn(move || {
         loop {
-            println!("thread 1 running");
+            // Send the result of get_degrees to other thread
             match tx.send(get_degrees(&mq)) {
                 Err(e) => println!("SenderError: {}", e),
-                Ok(_) => println!("Successfully send"),
+                Ok(_) => (),
             }
+            // Sleep to throttle
             thread::sleep(Duration::from_secs(1));
         }
     });
 
+    // Holds two points for every element (To form lines)
     let mut all_points: Vec<(Point, Point)> = Vec::new();
+    // Holds degrees of all lines to determine colors
     let mut all_degrees: Vec<f32> = Vec::new();
 
     while window.render() {
+        // get data from other thread async
         match rx.try_recv() {
             Ok(v) => {
                 degrees = v;
+                // Calculate x, y and add to array
                 let x: f32 = (degrees as f32).sin() * 1.0;
                 let z: f32 = (degrees as f32).cos() * 1.0;
                 all_points.push(((x, 0.0, z), (x, 1.0, z)));
@@ -58,12 +67,13 @@ fn main() {
             },
             Err(e) => println!("RecvError: {}", e),
         }
-        println!("{}", degrees);
         println!("Counter: {}", counter);
 
+        // Got trough whole array and print all the lines
         for i in 0..all_points.len() {
             let a = Point3::new(all_points[i].0.0, all_points[i].0.1, all_points[i].0.2);
             let b = Point3::new(all_points[i].1.0, all_points[i].1.1, all_points[i].1.2);
+            // Print in other color according to degree
             if all_degrees[i] >= 0.0 && all_degrees[i] <= 90.0 {
                 window.draw_line(&a, &b, &Point3::new(1.0, 0.0, 0.0));
             }else if all_degrees[i] > 90.0 && all_degrees[i] <= 180.0 {
--- server/server/src/main.rs
+++ server/server/src/main.rs
@@ -2,16 +2,17 @@
 #[macro_use] extern crate rocket;
 use posixmq::PosixMq;
 
-
+// Post route where the degrees are send
 #[post("/degree", data = "<degree>")]
 fn degree(degree: String) -> &'static str {
+    // Create messagequeue with name /degree 
     let mq = PosixMq::create("/degree").unwrap();
+    // Convert to integer and send degree
     mq.send(0, (degree.trim().parse::<f32>().unwrap()).to_string().as_bytes()).unwrap();
     return "OK";
 }
 
 fn main() {
-    // Setup messagequeue
     // Setup rocket
     rocket::ignite().mount("/", routes![degree]).launch();
 }
